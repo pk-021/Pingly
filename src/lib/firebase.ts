87e09d6bcb7bbd39d96df1208908a7978e4f013e
Prashansa <prashansashrestha1@gmail.com>
2025-08-13 16:17:44 +0000

    Access blocked: This app’s request is invalid prashansashrestha1@gmail.c

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,9 +9,9 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyA9XCsWRDMYKi4jVakH2ZwwCh5XQZ6AiEs",
-    authDomain: "pingly-web-bu4og.firebaseapp.com", // Update this too
-    projectId: "pingly-web-bu4og", // Change this
-    storageBucket: "pingly-web-bu4og.appspot.com", // And this
+    authDomain: "pingly-b1de9.firebaseapp.com",
+    projectId: "pingly-b1de9",
+    storageBucket: "pingly-b1de9.appspot.com",
     messagingSenderId: "1753113792496",
     appId: "1:1753113792496:web:2f0e35320853724a955747",
     measurementId: "G-9XG99F2H9E",
