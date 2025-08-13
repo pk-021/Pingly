9a3b4862ba1fb14e30d56dfa77870b4cd76cbf07
Prashansa <prashansashrestha1@gmail.com>
2025-08-13 16:21:33 +0000

    1/1 Next.js 15.3.3 (stale)Turbopack Runtime Error
    FirebaseError: Missin

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -36,12 +36,8 @@ export default function Dashboard() {
             </div>
             
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
-                <div className="lg:col-span-2 space-y-8">
+                <div className="lg:col-span-3 space-y-8">
                     <AnnouncementsCard />
-                    <DailyScheduleCard />
-                </div>
-                <div className="lg:col-span-1">
-                    <UpcomingTasksCard />
                 </div>
             </div>
         </div>
