61c93a057c529534e28d2c170403d8408d4e9864
Prashansa <prashansashrestha1@gmail.com>
2025-08-14 08:48:47 +0000

    studio-3961100899:~/studio{master}$ firebase deploy --only functions
    ==

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -7,15 +7,48 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendAnnouncementEmails = void 0;
 const v2_1 = require("firebase-functions/v2");
-const functions = require("firebase-functions");
-const logger = require("firebase-functions/logger");
+const functions = __importStar(require("firebase-functions"));
+const logger = __importStar(require("firebase-functions/logger"));
 // Import necessary modules for the announcement email function
-const admin = require("firebase-admin");
-const mail_1 = require("@sendgrid/mail");
+const admin = __importStar(require("firebase-admin"));
+const sgMail = __importStar(require("@sendgrid/mail"));
 // Initialize Firebase Admin SDK
 admin.initializeApp();
 const db = admin.firestore();
@@ -25,7 +58,7 @@ const db = admin.firestore();
 const sendgridApiKey = (_a = functions.config().sendgrid) === null || _a === void 0 ? void 0 : _a.key;
 // Set SendGrid API key
 if (sendgridApiKey) {
-    mail_1.default.setApiKey(sendgridApiKey);
+    sgMail.setApiKey(sendgridApiKey);
 }
 else {
     logger.error('SendGrid API key not configured in environment.');
@@ -83,7 +116,7 @@ exports.sendAnnouncementEmails = functions.firestore
                     // html: "<p>HTML version of the body</p>" // HTML body (optional)
                 };
                 // Add the email sending promise
-                emailPromises.push(mail_1.default.send(msg));
+                emailPromises.push(sgMail.send(msg));
             }
         });
         // Wait for all emails to be sent
